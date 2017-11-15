a29b749b5eb67e778e99eb92fa55528319b99425
Lyubomir <lyubomir.d.dimov@gmail.com>
2017-11-15 16:54:55 +0100

    INIT

---
+++ AleWebApp2/AleWebApp2/wwwroot/pages/jquery.widgets.js
@@ -0,0 +1,55 @@
+/**
+* Theme: Minton Admin Template
+* Author: Coderthemes
+* Component: Widget Chart
+*
+*/
+$(document).ready(function () {
+
+    DrawMouseSpeed = function () {
+        var mrefreshinterval = 500; // update display every 500ms
+        var lastmousex = -1;
+        var lastmousey = -1;
+        var lastmousetime;
+        var mousetravel = 0;
+        var mpoints = [];
+        var mpoints_max = 30;
+        $('html').mousemove(function (e) {
+            var mousex = e.pageX;
+            var mousey = e.pageY;
+            if (lastmousex > -1) {
+                mousetravel += Math.max(Math.abs(mousex - lastmousex), Math.abs(mousey - lastmousey));
+            }
+            lastmousex = mousex;
+            lastmousey = mousey;
+        });
+        var mdraw = function () {
+            var md = new Date();
+            var timenow = md.getTime();
+            if (lastmousetime && lastmousetime != timenow) {
+                var pps = Math.round(mousetravel / (timenow - lastmousetime) * 1000);
+                mpoints.push(pps);
+                if (mpoints.length > mpoints_max)
+                    mpoints.splice(0, 1);
+                mousetravel = 0;
+                
+            }
+            lastmousetime = timenow;
+            setTimeout(mdraw, mrefreshinterval);
+        }
+        // We could use setInterval instead, but I prefer to do it this way
+        setTimeout(mdraw, mrefreshinterval);
+    };
+
+
+    DrawMouseSpeed();
+
+    var resizeChart;
+
+    $(window).resize(function (e) {
+        clearTimeout(resizeChart);
+        resizeChart = setTimeout(function () {
+            DrawMouseSpeed();
+        }, 300);
+    });
+});
