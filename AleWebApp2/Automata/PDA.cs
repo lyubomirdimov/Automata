e4fb4ffe3627eca6d0c01ffc836929f59dd2075b
Lyubomir <lyubomir.d.dimov@gmail.com>
2018-01-06 19:23:40 +0100

    PDA accepts

--- AleWebApp2/Automata/PDA.cs
+++ AleWebApp2/Automata/PDA.cs
@@ -1,23 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Automata
 {
     public class PDA
     {
-        public List<string> States { get; set; }
         public List<char> InputAlphabet { get; set; }
         public List<char> StackAlphabet { get; set; }
-        public List<TransitionRelation> TransionRelations { get; set; }
+        public List<string> States { get; set; }
+        public List<TransitionFunction> Transitions { get; set; }
         public string InitialState { get; set; }
-        public char InitialStackSymbol { get; set; }
-        public List<string> FinalStates { get; set; }
+        public List<string> FinalStates { get; set; } = new List<string>();
 
-    
-        public PDA()
+        public bool Accepts(string input)
         {
+            // Check if input is in alphabet
+            foreach (char c in input)
+            {
+                if (InputAlphabet.Contains(c) == false)
+                    return false;
+            }
+
             
+            return Accepts(input, InitialState, new Stack<char>());
         }
+
+        private bool Accepts(string input, string currentState, Stack<char> stack)
+        {
+            // Iterations Completed, check if it is accepted
+            if (input.Length <= 0)
+                return IsFinalState(currentState) && !stack.Any();
+
+            char currentInput = input[0];
+            TransitionFunction function;
+            char popedSymbol;
+            List<char> pushSymbols;
+
+            if (stack.Any())
+            // 1. transition whose [symbol + pop stack] matches the [current input symbol + current top stack] (if the stack is not empty)
+            {
+                popedSymbol = stack.Pop();
+                function = Transitions.Find(x => x.StartState == currentState && x.InputSymbol == currentInput && x.StackTopSymbol == popedSymbol);
+                if (function != null)
+                {
+                    pushSymbols = function.PushSymbol.Where(c => c != Constants.Epsilon).ToList();
+                    pushSymbols.ForEach(stack.Push);
+                    return Accepts(input.Substring(1), function.EndState, stack);
+                }
+
+                // Push the poped symbol back to the stack
+                stack.Push(popedSymbol);
+            }
+            else
+            // 2.transition with epsilon pop stack whose symbol matches the current input symbol
+            {
+                function = Transitions.Find(x => x.StartState == currentState && x.InputSymbol == currentInput);
+                if (function != null)
+                {
+                    pushSymbols = function.PushSymbol.Where(c => c != Constants.Epsilon).ToList();
+                    pushSymbols.ForEach(stack.Push);
+                    return Accepts(input.Substring(1), function.EndState, stack);
+                }
+            }
+
+            if (stack.Any())
+            // 3.transition with epsilon symbol whose pop stack matches the current top stack(if the stack is not empty)
+            {
+                popedSymbol = stack.Pop();
+                function = Transitions.Find(x => x.StartState == currentState && x.InputSymbol == Constants.Epsilon && x.StackTopSymbol == popedSymbol);
+                if (function != null)
+                {
+                    pushSymbols = function.PushSymbol.Where(c => c != Constants.Epsilon).ToList();
+                    pushSymbols.ForEach(stack.Push);
+                    return Accepts(input.Substring(1), function.EndState, stack);
+                }
+
+                // Push the poped symbol back to the stack
+                stack.Push(popedSymbol);
+            }
+         
+            // 4.transition with epsilon symbol and epsilon pop stack
+            function = Transitions.Find(x => x.StartState == currentState && x.InputSymbol == Constants.Epsilon);
+            if (function != null)
+            {
+                pushSymbols = function.PushSymbol.Where(c => c != Constants.Epsilon).ToList();
+                pushSymbols.ForEach(stack.Push);
+                return Accepts(input.Substring(1), function.EndState, stack);
+            }
+
+            // Halted
+            return IsFinalState(currentState) && !stack.Any();
+        }
+
+
+        private bool IsFinalState(string state) => FinalStates.Contains(state);
     }
 }
---
+++ AleWebApp2/Automata/TransitionFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automata
+{
+    public class TransitionFunction
+    {
+        public string StartState { get; set; }
+        public char InputSymbol { get; set; }
+        public char StackTopSymbol { get; set; }
+        public List<char> PushSymbol { get; set; }
+        public string EndState { get; set; }
+
+    }
+}
